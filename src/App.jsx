61529fe9c60a570f9ad60c982dee6cd2602f79ae
mvpsheng <51054870+mvpsheng@users.noreply.github.com>
2023-06-27 07:50:55 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Nav from './components/Nav'
+import Subject from './components/Subject'
+export default function App() {
+  return (
+    <main>
+      <div><Nav></Nav></div>
+      <div><Subject></Subject></div>
+    </main>
+  )
+}
---
+++ src/components/Subject.jsx
@@ -0,0 +1,7 @@
+export default function Subject() {
+  return (
+    <main>
+      Subject
+    </main>
+  )
+}
